ef7b0bcb44143751eeea446a0d763e3a31710ec3
259841 <80674639+259841@users.noreply.github.com>
2021-04-30 20:36:03 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -4,14 +4,14 @@
  * @brief take analog input from potentiometer and store as digital value
  * @version 0.1
  * @date 2021-04-30
- *
+ * 
  * @copyright Copyright (c) 2021
- *
+ * 
  */
 #ifndef ACTIVITY2_H_INCLUDED
 #define ACTIVITY2_H_INCLUDED
 /**
- *@note avr header file
+ *@note avr header file 
  */
 
 #include <avr/io.h>
@@ -19,24 +19,17 @@
  * @note delay header file
  */
 #include <util/delay.h>
-#define RefToVcc (REFS0)
-#define EnableADC (ADEN)
-#define SetFCPU (ADPS0)
-#define MuxReg (ADMUX)
-#define CntrlReg (ADCSRA)
-#define StrtConv (ADSC)
-#define ADCInterruptFlag (ADIF)
-#define AtoDvalue (ADC)
+
 /**
  * @brief Initalising for ADC conversion
- *
+ * 
  */
 void initADC();
 /**
  * @brief convert the input analog value and return the digital value
- *
- * @param select_channel selects the channel(port) to connect for ADC conversion
- * @return uint16_t
+ * 
+ * @param select_channel selects the channel(port) to connect for ADC conversion 
+ * @return uint16_t 
  */
-uint16_t ReadADC(uint8_t select_channel);
+uint16_t ReadADC(uint8_t ch);
 #endif // ACTIVITY2_H_INCLUDED
