0b4ee0727514ccc77d36189b254daa33c3c229e2
259841 <80674639+259841@users.noreply.github.com>
2021-04-30 20:38:01 +0530

    Update activity3.h

--- inc/activity3.h
+++ inc/activity3.h
@@ -4,14 +4,14 @@
  * @brief Read input for Adc and display on oscilloscope
  * @version 0.1
  * @date 2021-04-30
- *
+ * 
  * @copyright Copyright (c) 2021
- *
+ * 
  */
 #ifndef ACTIVITY3_H_INCLUDED
 #define ACTIVITY3_H_INCLUDED
 /**
- *@note avr header file
+ *@note avr header file 
  */
 #include<avr/io.h>
 /**
@@ -22,20 +22,17 @@
 #define PrescaleTo64 (TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10))
 #define SetPWMoutputPin (DDRB|=(1<<PB1))
 #define PWMout (OCR1A)
-#define Pwm20percent (205)
-#define Pwm40percent (410)
-#define Pwm70percent (717)
-#define Pwm95percent (973)
+#define DUTY_CYCLE(Percent) (1024*Percent/100)
 
 /**
  * @brief Initalising for PWM
- *
+ * 
  */
-void initPWM();
+void initializePWM();
 /**
  * @brief to set PWM output depending on input
- *
- * @param value input from the analog temperature sensor(pot) converted to digital
+ * 
+ * @param value input from the analog temperature sensor(pot) converted to digital 
  * @return char temperature(Celsius) displayed through Usart on serial monitor
  */
 char PWMoutput(uint16_t value);
