283e741a41c81ef9eda8ac0c3c456f5a8debccdc
259841 <80674639+259841@users.noreply.github.com>
2021-04-30 20:43:33 +0530

    Update activity4.c

--- src/activity4.c
+++ src/activity4.c
@@ -1,19 +1,19 @@
 #include "activity4.h"
 
-void initUSART(uint16_t ubrr_value)
+void USARTInit(uint16_t ubrr_value)
 {
-    SetBaudLow;
-    SetBaudHigh;
-    SetCharBits;
-    EnableReceiverAndTransmitter;
+    UBRR0L=ubrr_value;
+    UBRR0H=(ubrr_value>>8)&0x00ff;
+    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 }
 
 
 void WriteCharUSART(char data)
 {
-    while(WaitForTransmissionReady)
+    while(!(UCSR0A&(1<<UDRE0)))
     {
         //Do nothing
     }
-    ReadOrWriteData=data;
+    UDR0=data;
 }
